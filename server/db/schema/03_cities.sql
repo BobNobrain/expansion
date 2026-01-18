cd12e2cd8bc47c4d30302bdd89b552e082147218
BobNobrain <bobnobrain@yandex.ru>
2026-01-19 02:07:00 +0300

    Add file order for schema/*.sql files
    The tables in different files start to reference each other, so the order must be established.

---
+++ server/db/schema/03_cities.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS cities (
+    id SERIAL PRIMARY KEY NOT NULL,
+    -- location
+    system_id CHAR(6) NOT NULL REFERENCES star_systems,
+    world_id VARCHAR(15) NOT NULL REFERENCES worlds,
+    tile_id SMALLINT NOT NULL,
+    -- city founding info
+    name TEXT NOT NULL,
+    established_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    established_by UUID NOT NULL,
+    -- city data
+    city_level SMALLINT NOT NULL DEFAULT 0,
+    city_data JSONB NOT NULL DEFAULT '{}'::JSONB,
+    population INTEGER NOT NULL DEFAULT 0,
+    population_data JSONB NOT NULL DEFAULT '{}'::JSONB
+);
--- server/db/schema/05_bases.sql
+++ server/db/schema/05_bases.sql
@@ -1,24 +1,7 @@
-CREATE TABLE IF NOT EXISTS cities (
-    id SERIAL PRIMARY KEY NOT NULL,
-    -- location
-    system_id CHAR(6) NOT NULL,
-    world_id VARCHAR(15) NOT NULL,
-    tile_id SMALLINT NOT NULL,
-    -- city founding info
-    name TEXT NOT NULL,
-    established_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    established_by UUID NOT NULL,
-    -- city data
-    city_level SMALLINT NOT NULL DEFAULT 0,
-    city_data JSONB NOT NULL DEFAULT '{}'::JSONB,
-    population INTEGER NOT NULL DEFAULT 0,
-    population_data JSONB NOT NULL DEFAULT '{}'::JSONB
-);
-
 CREATE TABLE IF NOT EXISTS bases (
     id SERIAL PRIMARY KEY NOT NULL,
-    system_id CHAR(6) NOT NULL,
-    world_id VARCHAR(15) NOT NULL,
+    system_id CHAR(6) NOT NULL REFERENCES star_systems,
+    world_id VARCHAR(15) NOT NULL REFERENCES worlds,
     tile_id SMALLINT NOT NULL,
     city_id INTEGER NOT NULL REFERENCES cities,
     company_id UUID NOT NULL REFERENCES companies,
